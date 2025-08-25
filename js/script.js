4155f53e79aaebc4373182e82d429355927b6841
Mariana Souza <69424128+marianavns@users.noreply.github.com>
2025-08-25 16:35:25 -0300

    [fix] Elimina chamada dupla no servidor e atualiza funcoes

--- js/script.js
+++ js/script.js
@@ -3,32 +3,31 @@ import { pontos } from './dataPins.js';
 let api_key;
 const isLocal = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
 
-verificaLocation();
+initAppByLocation();
 
-function verificaLocation(){
+function initAppByLocation(){
     if (isLocal) {
         import('../config.js').then(module => {
             api_key = module.API_KEY;
-            console.log("Rodando local");
-            mapContent();
+            console.log("Local running...");
+            createMapAndMarkers();
         }).catch(err => console.error("Erro ao importar config.js:", err));
     }
-    if (!isLocal){
+    if (!isLocal) {
         fetch('/.netlify/functions/getData')
             .then(res => res.json())
             .then(data => {
                 api_key = data.key;
-                console.log("Rodando no Netlify");
-                mapContent(); 
+                console.log("Netlify running...");
+                createMapAndMarkers(); 
             })
         .catch(err => console.error("Erro ao obter a chave:", err));
     }
 }
 
-function mapContent() {
-
+function createMapAndMarkers() {
     const zoomLevel = 14;
-    const map = criaMapa(api_key, zoomLevel, pontos);
+    const map = createMap(api_key, zoomLevel, pontos);
     const popupOffsets = {
         top: [0, 0],
         bottom: [0, -70],
@@ -36,49 +35,50 @@ function mapContent() {
         'bottom-left': [0, -70],
         left: [25, -35],
         right: [-25, -35]
-    };
-    
-    adicionarMarcadores(pontos, map, popupOffsets);
-    setInterval(() => atualizarImagem(pontos[0].ip), 60000);
-    atualizarImagem(pontos[0].ip);
+    };  
+    createMarkers(pontos, map, popupOffsets);
 }
 
-function criaMapa(api_key, zoomLevel, pontos) {
-    const map = tt.map({
+function createMap(api_key, zoomLevel, pontos) {
+    return tt.map({
         container: 'map',
         key: api_key,
         center: { lat: pontos[0].lat, lng: pontos[0].lng },
         zoom: zoomLevel
     });
-    return map;
 }
 
-function atualizarImagem(ip) {
-    const timestamp = new Date().getTime();
-    const novaUrl = `https://transito.serttel.com.br/cttupe/index.php/portal/getImg/${ip}/?t=${timestamp}`;
-    const tempImg = new Image();
-    tempImg.onload = function() {
-        console.log(`Dimensões da imagem: ${tempImg.naturalWidth} x ${tempImg.naturalHeight}`);
-        if (tempImg.naturalWidth > 1 && tempImg.naturalHeight > 1) {
-            document.getElementById("camera").src = novaUrl;
-            document.getElementById("modalImage").src = novaUrl;
-            document.getElementById("modalImage").alt = "";
+function captureImages(ip) {
+    return new Promise((resolve) => {
+        const timestamp = new Date().getTime();
+        const updatedImageURL = `https://transito.serttel.com.br/cttupe/index.php/portal/getImg/${ip}/?t=${timestamp}`;
+        const tempImg = new Image();
+
+        let timeout = setTimeout(() => {
+            console.log("Imagem demorando para responder...");
+        }, 10000);
+
+        tempImg.onload = () => {
+            clearTimeout(timeout);
+            document.getElementById("modalImage").src = updatedImageURL;
             document.getElementById("modalImage").style.display = "block";
-            let aviso = document.getElementById("avisoImagem");
-            if (aviso) aviso.style.display = "none";
-        } else {
-            mostrarAvisoImagem();
+            resolve(true);
         }
-    };
-    tempImg.onerror = function() {
-        console.log("Falha ao carregar a imagem.");
-        mostrarAvisoImagem();
-    };
-    tempImg.src = novaUrl;
+
+        tempImg.onerror = () => {
+            clearTimeout(timeout);
+            mostrarAvisoImagem();
+            resolve(false);
+        }
+
+        tempImg.src = updatedImageURL;
+    });
 }
+
+
 
 function mostrarAvisoImagem() {
-    document.getElementById("camera").src = "";
+    document.getElementById("modalImage").src = "";
     const modalImage = document.getElementById("modalImage");
     modalImage.src = "";
     modalImage.alt = "Imagem não encontrada";
@@ -97,25 +97,50 @@ function mostrarAvisoImagem() {
     }
 }
 
-function adicionarMarcadores(pinsInput, mapInput, popupInput){
+function createMarkers(pinsInput, mapInput, popupInput){
+    const modalEl = document.getElementById('cameraModal');
+    const modalContent = modalEl.querySelector('.modal-content');
+    const myModal = new bootstrap.Modal(modalEl);
+    let closeTimeout;
+    let openTimeout;
+
+    modalContent.addEventListener('mouseenter', () => {
+        clearTimeout(closeTimeout);
+    });
+
+    modalContent.addEventListener('mouseleave', () => {
+        closeTimeout = setTimeout(() => myModal.hide(), 200);
+    });
+
     pinsInput.forEach(ponto => {
         const marker = new tt.Marker().setLngLat({ lat: ponto.lat, lng: ponto.lng }).addTo(mapInput);
-
         const popup = new tt.Popup({ offset: popupInput }).setHTML(ponto.endereco);
         marker.setPopup(popup);
-
+        (async () => {
+            const resultado = await captureImages(ponto.ip);
+            console.log("Atualização finalizada: " + ponto.ip);
+                                        if (resultado === true) {
+                console.log("Imagem puxada com sucesso: " + resultado);
+            }
+            if (resultado === false) {
+                console.log("Imagem não encontrada: " + resultado);
+            }
+        })();
         const markerElement = marker.getElement();
+
         markerElement.addEventListener("mouseenter", () => {
-            atualizarImagem(ponto.ip);
-            document.getElementById('modalEndereco').textContent = ponto.endereco;
-            const myModal = new bootstrap.Modal(document.getElementById('cameraModal'));
-            myModal.show();
+            (async () => {
+                const resultado = await captureImages(ponto.ip);
+                console.log("Atualização finalizada: " + ponto.ip);
+            })();
+
+            clearTimeout(openTimeout);
+            openTimeout = setTimeout(() => myModal.show(), 200);
         });
 
         markerElement.addEventListener("mouseleave", () => {
-            const modalEl = document.getElementById('cameraModal');
-            const modal = bootstrap.Modal.getInstance(modalEl);
-            if (modal) modal.hide();
+            clearTimeout(openTimeout);
+            closeTimeout = setTimeout(() => myModal.hide(), 200);
         });
     });
 }
